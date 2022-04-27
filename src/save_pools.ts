a4b64cbbc5543f864a32c08bbbbf52abd1000b9f
liang xiong <liangxiong@okg.com>
2022-04-27 09:21:53 +0800

    add dayId filter for snapshots

--- src/save_pools.ts
+++ src/save_pools.ts
@@ -43,27 +43,44 @@ async function savePools(database: Database) {
   }
 }
 
-async function updatePoolWithLatestVolume(database: Database) {
-  const snapshots: DailyVolumeSnapshot[] =
-    await database.loadMany<DailyVolumeSnapshot>({}, snapshotCollectionName);
-  // get all pools using uniqby
-  const pools = _(snapshots)
-    .uniqBy((snapshot) => snapshot.pool.id)
-    .map((snapshot) => snapshot.pool)
-    .value();
-  const poolsWithLatestVolume = pools.map((pool) => {
-    // for each pool, update its volumeUSD with latest daily snapshot.
-    const latestSnapshot = _(snapshots)
-      .filter((snapshot) => snapshot.pool.id === pool.id)
-      .sortBy((snapshot) => snapshot.dayId)
-      .last() as DailyVolumeSnapshot;
-    return {
-      ...pool,
-      latestDailyVolumeUSD: latestSnapshot.volumeUSD,
-      latestDayId: latestSnapshot.dayId,
-    };
-  });
-  await database.saveMany(poolsWithLatestVolume, poolCollectionName);
+async function updatePoolWithLatestVolume(database: Database, dayId="") {
+  const protocols = [
+    // Protocol.Curve,
+    // Protocol.DODO,
+    // Protocol.BalancerV2,
+    Protocol.UniswapV2,
+    // Protocol.UniswapV3,
+  ];
+  for (let i=0;i<protocols.length;++i) {
+    const protocol = protocols[i];
+    logger.info(`processing indexer: ${Protocol[protocols[i]]}`);
+    const snapshots: DailyVolumeSnapshot[] =
+      await database.loadMany<DailyVolumeSnapshot>(
+        { "pool.protocol": protocol, dayId: {$gt: dayId}},
+        snapshotCollectionName
+      );
+    logger.info(`${snapshots.length} items of snapshots are found`);
+    // get all pools using uniqby
+    const pools = _(snapshots)
+      .uniqBy((snapshot) => snapshot.pool.id)
+      .map((snapshot) => snapshot.pool)
+      .value();
+    logger.info(`${pools.length} number of pools are found`);
+    const poolsWithLatestVolume = _(pools).map((pool) => {
+      // for each pool, update its volumeUSD with latest daily snapshot.
+      const latestSnapshot = _(snapshots)
+        .filter((snapshot) => snapshot.pool.id === pool.id)
+        .sortBy((snapshot) => snapshot.dayId)
+        .last() as DailyVolumeSnapshot;
+      return {
+        ...pool,
+        latestDailyVolumeUSD: latestSnapshot.volumeUSD,
+        latestDayId: latestSnapshot.dayId,
+      };
+    }).value();
+    logger.info(`updating pools with latest volumeUSD is done`);
+    await database.saveMany(poolsWithLatestVolume, poolCollectionName);
+  }
 }
 
 async function main() {
@@ -75,7 +92,7 @@ async function main() {
   await database.initDB(process.env.DB_NAME as string);
 
   await savePools(database);
-  await updatePoolWithLatestVolume(database);
+  await updatePoolWithLatestVolume(database, "19000");
 
   await database.close();
 }
